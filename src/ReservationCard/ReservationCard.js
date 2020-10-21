fbe0bacd875966d50c63e7b34c160f2111a0ac4f
Naomi Ware <nware1066@yahoo.com>
2020-10-21 10:40:17 -0600

    Add ReservationCard component

---
+++ src/ReservationCard/ReservationCard.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function ReservationCard(props) {
+  return {
+    <article className='reservation-card'>
+      <p>name: {props.reservation.name}</p>
+      <p>date: {props.reservation.date}</p>
+      <p>time: {props.reservation.time}</p>
+    </article>
+  }
+}
+//
